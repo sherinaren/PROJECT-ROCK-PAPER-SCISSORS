3262ecd733dd7244603052684ac2f227fc88691a
Sherina Ren <sherinaren@gmail.com>
2022-04-25 00:42:42 -0500

    I did it! Thank you Ben <3

--- javascript.js
+++ javascript.js
@@ -1,1 +1,58 @@
-console.log('ben is poo poo pants')
+function computerPlay() 
+{
+    let rng = Math.random(); 
+    if (rng<=.33) {
+        return 'rock';
+    } else if (rng<=.66) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+};
+
+function playRound(playerSelection, computerSelection) {
+    let r = 'rock';
+    let p = 'paper'; 
+    let s = 'scissors';
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return 'You\'re both winners!';
+    } else if (playerSelection ===  r) {
+        if (computerSelection === p) {
+            return 'You lost'
+        } else {return "You win"};
+    } else if (playerSelection === s) {
+        if (computerSelection === r) {
+            return 'You lost'
+        } else {return "You win"};
+    } else if (playerSelection === p) {
+        if (computerSelection === s) {
+            return 'You lost'
+        } else {return "You win"};
+    } 
+  };
+  
+function game() {
+    let score = 0;
+    let cscore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("What move do you want to make?");
+        let computer = computerPlay();
+        let result = playRound(playerSelection, computer); 
+        console.log(result);
+        if (result === "You win" && "You're both winners!") {score++};
+        if (result === "You lost" && "You're both winners!") {cscore++};
+    }
+    if (score > cscore) {
+        console.log("Yay! You beat the computer");
+    } else if (score === cscore) {
+        console.log("Woohoo! You're both winners");
+    } else {
+        console.log("Boohoo! You lost!");
+    }
+
+};
+
+game()
+
+
